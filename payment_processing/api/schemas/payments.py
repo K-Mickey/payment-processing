f8d4f7deaab2130bfefb6a1fe543b856281789c6
Mikhail Kokuev <kokmickey@yandex.ru>
2026-04-18 21:41:48 +0500

    replace comprehensions to the collection constructors

--- payment_processing/api/schemas/payments.py
+++ payment_processing/api/schemas/payments.py
@@ -19,7 +19,7 @@ class CreatePaymentRequest(BaseModel):
 class CreatePaymentResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     payment_id: UUID = Field(title="Payment ID", examples=["1e4e5e6e-7e8e-9e9e-aeae-1e2e3e4e5e6e"])
-    status: PaymentStatus = Field(title="Payment status", examples=[status for status in PaymentStatus])
+    status: PaymentStatus = Field(title="Payment status", examples=list(PaymentStatus))
     created_at: datetime = Field(title="Payment created at", examples=["2021-01-01T00:00:00Z"])
 
 
@@ -30,7 +30,7 @@ class PaymentResponse(BaseModel):
     currency: CurrencyCode = Field(title="Payment currency", examples=[code for code in CurrencyCode])
     description: str = Field(title="Payment description", examples=["Payment for order #123"])
     metadata: dict = Field(title="Payment metadata", examples=[{"order_id": "123"}])
-    status: PaymentStatus = Field(title="Payment status", examples=[status for status in PaymentStatus])
+    status: PaymentStatus = Field(title="Payment status", examples=list(PaymentStatus))
     idempotency_key: str = Field(title="Payment idempotency key", examples=["1e4e5e6e-7e8e-9e9e-aeae-1e2e3e4e5e6e"])
     webhook_url: str = Field(title="Payment webhook URL", examples=["https://example.com/webhook"])
     created_at: datetime = Field(title="Payment created at", examples=["2021-01-01T00:00:00Z"])
